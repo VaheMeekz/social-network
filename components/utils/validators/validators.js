ad7c41a48987ddc8ebe0888f8cc9c4c7547bc281
VaheMeekz <71688163+VaheMeekz@users.noreply.github.com>
2021-08-23 19:52:54 +0400

    Delete components directory

--- components/utils/validators/validators.js
+++
@@ -1,8 +0,0 @@
-export const required = value =>{
-    if (value) return undefined;
-    return 'Fiels is required';
-}
-export const maxLenghtCreator = (maxLenght) => (value) =>{
-    if (value.length > maxLenght) return('Max lenght is' + maxLenght + 'symbols');
-    return undefined;
-}
